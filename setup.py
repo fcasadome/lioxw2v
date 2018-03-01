9b31973f65e7c2ac08e5bfdcc0a08fab1d6f848b
fcasadome <francisco.casadome@lionbridge.com>
2018-03-01 18:35:48 +0100

    intial

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='lioxw2v',
+      version='0.1',
+      description='Lionbridge Word2vec',
+      url='http://github.com/fcasadome/lioxw2v',
+      author='Francisco Casadome',
+      author_email='francisco.casadome@lionbridge.com',
+      license='MIT',
+      packages=['lioxw2v'],
+      zip_safe=False)
